17410b6f01f14db8b8849de686b2707557ff19b4
AzureCloud9876 <azurecloud9876@gmail.com>
2023-05-10 18:24:39 +0200

    updated braces and brackets

--- function.js
+++ function.js
@@ -2,4 +2,21 @@ function elements(a, b) {
   return [a + b, (a + b) / 2, a - b];
 }
 let [sum, avg, diff] = elements(20, 10);
-console.log(sum, avg, diff);
+console.log(sum, avg, diff); //30 15 10
+
+
+function getPersonWithBraces(name,age,gender) {
+  return {
+    name,age,gender
+  }
+}
+
+console.log(getPersonWithBraces("pavan",35,"male")) //{ name: 'pavan', age: 35, gender: 'male' }
+
+function getPersonWithBracket(name,age,gender) {
+  return [name,age,gender]
+    
+}
+
+console.log(getPersonWithBracket("pavan",35,"male")) //[ 'pavan', 35, 'male' ]
+
