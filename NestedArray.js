0d395506db1612944e30f7a3ab3fb893fb4de82e
azurecloud9876 <110204366+azurecloud9876@users.noreply.github.com>
2023-05-10 19:35:31 +0200

    Update NestedArray.js

--- NestedArray.js
+++ NestedArray.js
@@ -2,4 +2,4 @@ const array = ["paaaaaa", ["keeeeee", "jeeeeeeeee", "gaeeeeeeee"]];
 const [x, [y, z]] = array;
 console.log(x); //paaaaaa
 console.log(y); //keeeeee
-console.log(z);//jeeeeeeeee
+console.log(z);//jeeeeeeeeee
